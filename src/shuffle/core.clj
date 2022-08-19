e5944353b269f64cf784dded81ec10a61bd92e44
Mark Mestern <mark.mestern@uswitch.com>
2022-08-19 09:58:03 +0100

    Add fisher yates

--- src/shuffle/core.clj
+++ src/shuffle/core.clj
@@ -29,9 +29,11 @@
 
 (naive-shuffle cards-52)
 
-(defn demo [shuffle-f items x]
+(defn demo
+  "Print how often an item is is 0th position"
+  [shuffle-f items x]
   (println (into (sorted-map)
-                 (frequencies (for [i (range x)]
+                 (frequencies (for [_ (range x)]
                                 (get (shuffle-f items) 0))))))
 
 
@@ -42,24 +44,19 @@
 (doseq [_ (range 1)]
   (demo naive-shuffle cards-52 5200))
 
-
-(defn  good-shuffle
+(defn good-shuffle ;; Fisher Yates as per Knuth
   [cards-to-shuffle]
   (let [cards-to-shuffle (vec cards-to-shuffle) ;; convert to vector for get
         n                (count cards-to-shuffle)]
-    (loop [i     0
-           cards []]
-      (let [j (rand-int (inc i))]
-        (if (= i n)
+    (loop [i     (dec n)
+           cards cards-to-shuffle]
+      (let [j (rand-int (inc i))] ;; 0 ≤ j ≤ i
+        (if (= i 0)
           cards
-          (recur (inc i)
-                 (if (= i j)
-                   (assoc cards
-                          j (get cards-to-shuffle i)) ;; insert new card at j, uses original source
-                   (assoc cards
-                          i (get cards j)            ;; move j to i
-                          j (get cards-to-shuffle i) ;; insert new card at j, uses original source
-                          ))))))))
+          (recur (dec i)
+                 (assoc cards
+                        i (get cards j) ;; swap i,j positions
+                        j (get cards i))))))))
 
 
 ;; use this (java implementation)
@@ -72,10 +69,10 @@
 ;; DEMO goodness
 (doseq [i (range 1)]
   (demo shuffle items-4 1000))
-(doseq [i (range 1)]
+(doseq [i (range 10)]
   (demo good-shuffle items-4 1000))
 (doseq [i (range 1)]
-  (demo good-shuffle cards-52 5200))
+  (demo good-shuffle cards-52 52000))
 
 (defn -main
   "I don't do a whole lot ... yet."

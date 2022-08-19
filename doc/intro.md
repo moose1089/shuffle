bc1f1d46fa37c474b1f1605f2290781807cdebd1
Mark Mestern <mark.mestern@uswitch.com>
2022-08-19 10:56:55 +0100

    neaten

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to shuffle
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- src/shuffle/core.clj
+++ src/shuffle/core.clj
@@ -6,6 +6,7 @@
         card [2 3 4 5 6 7 8 9 "T" "J" "Q" "K" "A"]]
     (str card suit)))
 
+(def items-3 [1 2 3])
 (def items-4 [1 2 3 4])
 
 (defn naive-shuffle
@@ -41,24 +42,43 @@
 ;; DEMO badness
 (println "=============")
 (doseq [_ (range 1)]
-  (demo naive-shuffle items-4 1000))
+  (demo naive-shuffle items-3 1000000))
 (doseq [_ (range 1)]
   (demo naive-shuffle cards-52 5200))
+
+;; But why is it bad? Naive shuffle produces "n to the power n" non distinct
+;; arrangements with equal probability e.g. for 3 items this is 27 arrangements.
+
+;; The actual number of distinct arrangements of 3 items is 3! = 6, but
+;; 27 / 6 = 4 rem 3. i.e. no matter how the arrangements fall into the 6 distinct
+;; arrangements, it is not possible for these to form a probablity of 1/6, because
+;; 6 is not a factor of 27.
+;;
+;; In this case,
+;; [1, 2, 3] occurs 4/27 times != 1/6
+;; [3, 1, 2] occurs 4/27 times != 1/6
+;; [3, 2, 1] occurs 4/27 times != 1/6
+;; [2, 1, 3] occurs 5/27 times != 1/6
+;; [2, 3, 1] occurs 5/27 times != 1/6
+;; [1, 3, 2] occurs 5/27 times != 1/6
+
 
 (defn good-shuffle ;; Fisher Yates as per Knuth
   [cards-to-shuffle]
   (let [cards-to-shuffle (vec cards-to-shuffle) ;; convert to vector for get
         n                (count cards-to-shuffle)]
     (loop [i     (dec n)
-           cards cards-to-shuffle]
-      (let [j (rand-int (inc i))] ;; 0 ≤ j ≤ i
-        (if (= i 0)
-          cards
+           cards cards-to-shuffle]    ;; 1 ≤ i ≤ n-1
+      (if (= i 0)
+        cards
+        (let [j (rand-int (inc i))]   ;; 0 ≤ j ≤ i
           (recur (dec i)
                  (assoc cards
                         i (get cards j) ;; swap i,j positions
                         j (get cards i))))))))
 
+;; Why does it work? => It can produce n! arrangements with equal probablity.
+;; These correspond to the n! distinct permutations.
 
 ;; use this (java implementation)
 (shuffle cards-52)
@@ -66,7 +86,7 @@
 ;; or this
 (good-shuffle cards-52)
 
-(good-shuffle items-4)
+(good-shuffle items-3)
 
 ;; DEMO goodness
 (doseq [i (range 1)]
@@ -74,7 +94,7 @@
 (doseq [i (range 10)]
   (demo good-shuffle items-4 1000))
 (doseq [i (range 1)]
-  (demo good-shuffle cards-52 52000))
+  (demo good-shuffle cards-52 5200))
 
 (defn -main
   [& args])
